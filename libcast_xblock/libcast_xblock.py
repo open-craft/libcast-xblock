1cd9e0d14cbbfb26561f2e5ff97e8e56d5215a26
Tim Krones <tim@opencraft.com>
2018-01-17 10:47:50 +0100

    Temporarily disable videofront.

--- libcast_xblock/libcast_xblock.py
+++ libcast_xblock/libcast_xblock.py
@@ -100,13 +100,7 @@ class LibcastXBlock(StudioEditableXBlockMixin, XBlock):
         http://stackoverflow.com/questions/35138642/ms-edge-video-cross-origin-subtitles-fail
         As a consequence, we need to download the subtitle file server-side.
         """
-        from videoproviders.subtitles import get_vtt_content
-
-        subtitle_url = request.GET.get('url')
-        if not subtitle_url:
-            return webob.Response(status=404)
-        caps = get_vtt_content(subtitle_url) or ""
-        return webob.Response(caps, content_type='text/vtt')
+        return webob.Response(status=404)
 
     def get_icon_class(self):
         """CSS class to be used in courseware sequence list."""
@@ -124,97 +118,7 @@ class LibcastXBlock(StudioEditableXBlockMixin, XBlock):
         fragment = Fragment()
         if self.is_youtube_video and self.video_id:
             self.get_youtube_content(fragment)
-        else:
-            self.get_videofront_content(fragment)
         return fragment
-
-    def get_videofront_content(self, fragment):
-        from videoproviders.api import videofront
-
-        template_content = self.resource_string("public/html/videofront.html")
-        template = Template(template_content)
-        messages = []  # tuple list
-        context = {
-            'display_name': self.display_name,
-            'video_id': self.resource_slug,
-            'messages': messages,
-            'allow_download': self.allow_download,
-            'downloads': [],
-            'sources': [],
-            'subtitles': [],
-            'thumbnail_url': '',
-            'transcript_root_url': self.transcript_root_url(),
-        }
-        if self.resource_slug:
-            try:
-                videofront_client = videofront.Client(self.course_key_string)
-                video = videofront_client.get_video_with_subtitles(self.resource_slug)
-                context['subtitles'] = video['subtitles']
-                context['thumbnail_url'] = video['thumbnail_url']
-
-                # Sort video sources by decreasing resolution
-                video_sources = sorted(video['video_sources'], key=lambda s: -s['res'])
-
-                # Streaming sources
-                source_labels = {
-                    'HD': '1080p',
-                    'SD': '720p',
-                    'LD': '480p',
-                    'UL': '240p',
-                }
-                context['sources'] = [
-                    {
-                        'url': source['url'],
-                        'label': source_labels.get(source['label'], source['label']),
-                        'res': source['res'],
-                    }
-                    for source in video_sources
-                ]
-
-                # Download links
-                download_labels = {
-                    'HD': 'Haute (1080p)',
-                    'SD': 'Normale (720p)',
-                    'LD': 'Mobile (480p)',
-                    'UL': 'Basse (240p)',
-                }
-                context['downloads'] = [
-                    {
-                        'url': source['url'],
-                        'label': download_labels.get(source['label'], source['label'])
-                    }
-                    for source in video_sources
-                ]
-
-            except videofront.MissingCredentials as e:
-                messages.append(('error', e.verbose_message))
-            except videofront.ClientError as e:
-                # Note that we may not log an exception here, because unicode
-                # messages cannot be encoded by the logger
-                logger.error(e.message)
-                messages.append(('error', ugettext_lazy("An unexpected error occurred.")))
-        else:
-            messages.append(('warning', ugettext_lazy(
-                "You need to define a valid video ID. "
-                "Video IDs for your course can be found in the video upload"
-                " dashboard."
-            )))
-
-        content = template.render(Context(context))
-        fragment.add_content(content)
-
-        fragment.add_css(self.resource_string("public/css/style.css"))
-        # Load video-js stylesheet from FUN; if this becomes a problem, we can
-        # always copy the stylesheet to the xblock.
-        fragment.add_css_url(staticfiles_storage.url("fun/js/vendor/videojs/video-js.min.css"))
-        fragment.add_javascript(self.resource_string("public/js/videofront.js"))
-
-        fragment.initialize_js("VideoXBlock", json_args={
-            'course_id': self.course_key_string,
-            'video_id': self.resource_slug,
-            'adways_id': self.adways_id,
-            'block_id': self.location.block_id,
-        })
 
     def get_youtube_content(self, fragment):
         # iframe element id
